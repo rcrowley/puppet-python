6414924be01d92640cd8e97f95d0eb134ecfbe2c
Richard Crowley <r@rcrowley.org>
2009-08-30 23:11:36 +0000

    Refactored php_install and python_install out to the new sourceinstall module.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,54 +1,3 @@
-define python_install($version) {
-
-	file { "/opt/Python-$version.tar.bz2":
-		before => Exec["extract-Python-$version"],
-		source => "puppet://$servername/python/Python-$version.tar.bz2",
-		ensure => present,
-	}
-	exec { "extract-Python-$version":
-		require => File["/opt/Python-$version.tar.bz2"],
-		before => Exec["configure-Python-$version"],
-		cwd => "/root",
-		command => "tar xjf /opt/Python-$version.tar.bz2",
-		creates => "/root/Python-$version",
-		unless => "test -d /opt/Python-$version",
-	}
-
-	exec { "configure-Python-$version":
-		require => Exec["extract-Python-$version"],
-		before => Exec["make-Python-$version"],
-		cwd => "/root/Python-$version",
-		command => "./configure --prefix=/opt/Python-$version $flags",
-		timeout => "-1",
-		creates => "/root/Python-$version/Makefile",
-		onlyif => "test -d /root/Python-$version",
-	}
-	exec { "make-Python-$version":
-		require => Exec["configure-Python-$version"],
-		before => Exec["install-Python-$version"],
-		cwd => "/root/Python-$version",
-		command => "make",
-		timeout => "-1",
-		creates => "/root/Python-$version/Python",
-		onlyif => "test -d /root/Python-$version",
-	}
-	exec { "install-Python-$version":
-		require => Exec["make-Python-$version"],
-		before => Exec["remove-Python-$version"],
-		cwd => "/root/Python-$version",
-		command => "make install",
-		creates => "/opt/Python-$version",
-		onlyif => "test -d /root/Python-$version",
-	}
-
-	exec { "remove-Python-$version":
-		cwd => "/root",
-		command => "rm -rf Python-$version",
-		onlyif => "test -d /root/Python-$version",
-	}
-
-}
-
 class python {
 	include python::python_3_1
 	include python::python_2_6_2
@@ -56,13 +5,31 @@ class python {
 }
 
 class python::python_3_1 {
-	python_install { "Python-3.1": version => "3.1" }
+	sourceinstall { "Python-3.1":
+		module => "python",
+		package => "Python",
+		version => "3.1",
+		flags => "",
+		bin => "python",
+	}
 }
 
 class python::python_2_6_2 {
-	python_install { "Python-2.6.2": version => "2.6.2" }
+	sourceinstall { "Python-2.6.2":
+		module => "python",
+		package => "Python",
+		version => "2.6.2",
+		flags => "",
+		bin => "python",
+	}
 }
 
 class python::python_2_5_4 {
-	python_install { "Python-2.5.4": version => "2.5.4" }
+	sourceinstall { "Python-2.5.4":
+		module => "python",
+		package => "Python",
+		version => "2.5.4",
+		flags => "",
+		bin => "python",
+	}
 }
