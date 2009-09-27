f75c3b875c939c7246f6c2c667bfbda0aae158a8
Richard Crowley <r@rcrowley.org>
2009-09-27 22:07:09 +0000

    Cleaning up the Pip installer.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,9 @@
 define pipinstall($version, $bin) {
 	exec { "install-pip-0.4-$version":
 		require => Exec["extract-pip-0.4"],
-		before => Exec["remove-pip-0.4"],
 		cwd => "/root/pip-0.4",
 		command => "/opt/Python-$version/bin/$bin setup.py install",
 		creates => "/opt/Python-$version/bin/pip",
-		onlyif => "test -d /opt/Python-$version",
 	}
 }
 
@@ -14,33 +12,30 @@ class python {
 	include python::python_2_6_2
 	include python::python_2_5_4
 
-	file { "/opt/pip-0.4.tar.gz":
+	file { "/root/pip-0.4.tar.gz":
 		require => [
 			Sourceinstall["Python-3.1"],
 			Sourceinstall["Python-2.6.2"],
 			Sourceinstall["Python-2.5.4"]
 		],
-		before => Exec["extract-pip-0.4"],
 		source => "puppet://$servername/python/pip-0.4.tar.gz",
 		ensure => present,
 	}
 	exec { "extract-pip-0.4":
-		require => File["/opt/pip-0.4.tar.gz"],
-		before => [
-			Pipinstall["pip-0.4-3.1"],
-			Pipinstall["pip-0.4-2.6.2"],
-			Pipinstall["pip-0.4-2.5.4"]
-		],
+		require => File["/root/pip-0.4.tar.gz"],
 		cwd => "/root",
-		command => "tar xf /opt/pip-0.4.tar.gz",
+		command => "tar xf /root/pip-0.4.tar.gz",
 		creates => "/root/pip-0.4",
 		#unless => "UNLESS WHAT???",
 	}
 	# If anything happened in order, each pipinstall would go here
 	exec { "remove-pip-0.4":
-		cwd => "/root",
-		command => "rm -rf pip-0.4",
-		onlyif => "test -d /root/pip-0.4",
+		require => [
+			Pipinstall["pip-0.4-3.1"],
+			Pipinstall["pip-0.4-2.6.2"],
+			Pipinstall["pip-0.4-2.5.4"]
+		],
+		command => "rm -rf /root/pip-0.4*",
 	}
 
 	file { "/usr/local/bin/pick-python":
