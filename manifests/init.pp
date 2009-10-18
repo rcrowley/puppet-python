afd0ed5c99d47d974922f17e3a186fe7e2908187
Richard Crowley <r@rcrowley.org>
2009-10-18 01:39:13 +0000

    Added setuptools installation so Pip will work properly.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,38 @@
+class setuptoolsinstall::build {
+	file { "/usr/local/sbin/setuptoolsinstall":
+		source => "puppet://$servername/python/setuptoolsinstall",
+		ensure => present,
+	}
+}
+
+define setuptoolsinstall($egg, $prefix, $version) {
+	include setuptoolsinstall::build
+	exec { "/usr/local/sbin/setuptoolsinstall $egg $prefix >>/tmp/setuptoolsinstall.out 2>>/tmp/setuptoolsinstall.err":
+		require => [
+			Sourceinstall["Python-$version"],
+			File["/usr/local/sbin/setuptoolsinstall"]
+		],
+		timeout => "-1",
+	}
+}
+
 define pipinstall($version, $bin) {
 	exec { "pipinstall-exec-$version":
-		require => Exec["pipinstall-extract"],
-		cwd => "/root/pip-0.4",
+		require => [
+			Sourceinstall["Python-$version"],
+			Setuptoolsinstall["setuptoolsinstall-$version"],
+			Exec["pipinstall-extract"]
+		],
+		cwd => "/tmp/pip-0.5.1",
 		command => "/opt/Python-$version/bin/$bin setup.py install",
 		creates => "/opt/Python-$version/bin/pip",
+	}
+}
+
+define pip($package, $version) {
+	exec { "pip-exec-$package-$version":
+		require => Pipinstall["pipinstall-$version"],
+		command => "/opt/Python-$version/bin/pip install $package",
 	}
 }
 
@@ -12,30 +41,24 @@ class python {
 	include python::python_2_6_2
 	include python::python_2_5_4
 
-	file { "/root/pip-0.4.tar.gz":
-		require => [
-			Sourceinstall["Python-3.1"],
-			Sourceinstall["Python-2.6.2"],
-			Sourceinstall["Python-2.5.4"]
-		],
-		source => "puppet://$servername/python/pip-0.4.tar.gz",
-		ensure => present,
+	exec { "pipinstall-fetch":
+		cwd => "/tmp",
+		command => "wget http://pypi.python.org/packages/source/p/pip/pip-0.5.1.tar.gz",
 	}
 	exec { "pipinstall-extract":
-		require => File["/root/pip-0.4.tar.gz"],
-		cwd => "/root",
-		command => "tar xf /root/pip-0.4.tar.gz",
-		creates => "/root/pip-0.4",
+		require => Exec["pipinstall-fetch"],
+		cwd => "/tmp",
+		command => "tar xf /tmp/pip-0.5.1.tar.gz",
+		creates => "/tmp/pip-0.5.1",
 		#unless => "UNLESS WHAT???",
 	}
 	# If anything happened in order, each pipinstall would go here
 	exec { "pipinstall-remove":
 		require => [
-			Pipinstall["pipinstall-3.1"],
 			Pipinstall["pipinstall-2.6.2"],
 			Pipinstall["pipinstall-2.5.4"]
 		],
-		command => "rm -rf /root/pip-0.4*",
+		command => "rm -rf /tmp/pip-0.5.1*",
 	}
 
 	file { "/usr/local/bin/pick-python":
@@ -52,10 +75,6 @@ class python::python_3_1 {
 		prefix => "/opt/Python-$version",
 		flags => "",
 	}
-	pipinstall { "pipinstall-$version":
-		version => "$version",
-		bin => "python3",
-	}
 	file { "/opt/Python-$version/bin/python":
 		require => Sourceinstall["Python-$version"],
 		ensure => "python3",
@@ -69,9 +88,18 @@ class python::python_2_6_2 {
 		prefix => "/opt/Python-$version",
 		flags => "",
 	}
+	setuptoolsinstall { "setuptoolsinstall-$version":
+		egg => "http://pypi.python.org/packages/2.6/s/setuptools/setuptools-0.6c9-py2.6.egg",
+		prefix => "/opt/Python-$version",
+		version => "$version",
+	}
 	pipinstall { "pipinstall-$version":
 		version => "$version",
 		bin => "python",
+	}
+	pip { "virtualenv-$version":
+		package => "virtualenv",
+		version => "$version",
 	}
 }
 
@@ -82,8 +110,17 @@ class python::python_2_5_4 {
 		prefix => "/opt/Python-$version",
 		flags => "",
 	}
+	setuptoolsinstall { "setuptoolsinstall-$version":
+		egg => "http://pypi.python.org/packages/2.5/s/setuptools/setuptools-0.6c9-py2.5.egg",
+		prefix => "/opt/Python-$version",
+		version => "$version",
+	}
 	pipinstall { "pipinstall-$version":
 		version => "$version",
 		bin => "python",
+	}
+	pip { "virtualenv-$version":
+		package => "virtualenv",
+		version => "$version",
 	}
 }
